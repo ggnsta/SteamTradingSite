ec5f10b4746ab15ffac56a02e9985c46c5b585b9
Kluev Egor 650502 <39225304+ggnsta@users.noreply.github.com>
2021-04-11 21:10:55 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/MainController.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "Главная страница");
+        return "home";
+    }
+
+    @GetMapping("/SteamAuthRedirect")
+    public String stemAuthRedirect(Model model) {
+        return "SteamAuthRedirect";
+    }
+
+    @GetMapping("/test")
+    public String ttest(Model model) {
+        return "test";
+    }
+
+}
